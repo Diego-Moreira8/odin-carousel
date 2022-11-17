a0ed4a1b616700b2cc073536c4f318a10c0b55ad
Diego <diego.moreira529@gmail.com>
2022-11-16 21:44:25 -0300

    creating function for next btn

--- index.js
+++ index.js
@@ -1,27 +1,54 @@
-const previousBtn = document.querySelector("#previous-button");
-const nextBtn = document.querySelector("#next-button");
-const jumpToBtns = document.querySelectorAll(".jump-to-button");
+const carousel = (() => {
+  const carousel = document.querySelector("#carousel");
 
-function carousel() {
+  const carouselSlides = carousel.querySelector("#carousel-slides");
+  let allSlides = carousel.querySelectorAll(".carousel-slide");
+
+  carousel.querySelector("#next-button").addEventListener("click", () => {
+    if (currentSlide > slidesURLs.length) {
+      showSlide((currentSlide = 0));
+    } else {
+      showSlide(++currentSlide);
+    }
+  });
+  const jumpToButtons = carousel.querySelector("#jump-to-buttons");
+
   let slidesURLs = [];
-
   let currentSlide = 0;
 
-  function changeSlide() {
-    if (currentSlide >= slides.length) {
-      currentSlide = 0;
-    } else if (currentSlide < 0) {
-      currentSlide = slides.length - 1;
+  function renderSlides() {
+    jumpToButtons.innerHTML = "";
+    carouselSlides.innerHTML = "";
+
+    for (let i = 0; i < slidesURLs.length; i++) {
+      carouselSlides.insertAdjacentHTML(
+        "beforeend",
+        `<div class="carousel-slide" data-slide-index="${i}"
+        style="background-image: url(${slidesURLs[i]})"></div>`
+      );
+      jumpToButtons.insertAdjacentHTML(
+        "beforeend",
+        `<button type="button" class="jump-to-button" 
+        data-btn-index="${i}">O</button>`
+      );
     }
-    for (let slide of slides) {
-      slide.classList.remove("active");
-    }
-    slides[currentSlide].classList.add("active");
+    allSlides = carousel.querySelectorAll(".carousel-slide");
   }
 
-  function nextSlide() {}
+  function addSlide(url) {
+    slidesURLs.push(url);
+    renderSlides();
+  }
 
-  function prevSlide() {}
+  function showSlide(index) {
+    allSlides.forEach((slide) => slide.classList.remove("active"));
+    allSlides[index].classList.add("active");
+  }
 
-  return { changeSlide, nextSlide, prevSlide };
-}
+  return { addSlide };
+})();
+
+carousel.addSlide("1");
+carousel.addSlide("2");
+carousel.addSlide("3");
+carousel.addSlide("4");
