cd5cb166383ae8a7598b03ca00d47d3288ebbac5
Diego <diego.moreira529@gmail.com>
2022-11-17 20:59:53 -0300

    CSS for dropdown submenu

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+import navBar from "./header.js";
 import carousel from "./carousel.js";
+
+navBar();
 
 carousel.addSlides([
   "https://images.unsplash.com/photo-1667986968934-bf1fd9db241f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80",
