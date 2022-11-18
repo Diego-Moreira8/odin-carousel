183e7f166a85ebf84886f803266e17b3bcfc1a7d
Diego <diego.moreira529@gmail.com>
2022-11-17 21:13:35 -0300

    logic for dropdown on mouse enter

--- header.js
+++ header.js
@@ -1,1 +1,11 @@
-export default function navBar() {}
+export default function navBar() {
+  const dropdowns = document.querySelectorAll(".dropdown");
+
+  dropdowns.forEach((el) => el.addEventListener("mouseenter", toggleDropdown));
+  dropdowns.forEach((el) => el.addEventListener("mouseleave", toggleDropdown));
+
+  function toggleDropdown(e) {
+    const container = e.target.querySelector(".dropdown-container");
+    container.classList.toggle("active");
+  }
+}
