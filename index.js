214baf42aaf40e7b4977fb3e3546016f7eedb91e
Diego Moreira de Oliveira <diego.moreira529@gmail.com>
2024-03-21 21:05:47 -0300

    feat: change slide with delay

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 class Carousel {
-  constructor(parentElement, imgLinks = []) {
+  constructor(parentElement, imgLinks = [], delay) {
     this.parentElement = parentElement;
     this.imgLinks = [...imgLinks];
+    this.delay = delay;
 
     this.imgElements = this.generateImgElements();
     this.content = document.createElement("div");
     this.currentSlide = 0;
+    this.intervalId;
   }
 
-  renderCurrentSlide() {
+  renderCurrentSlide(event) {
     const currentSlideElement =
       this.content.querySelector(".content > .active");
 
@@ -17,21 +19,24 @@ class Carousel {
     }
 
     this.imgElements[this.currentSlide].classList.add("active");
+
+    // Restart only when a slide is selected by the user
+    if (event) this.restartSlideInterval();
   }
 
-  nextSlide() {
-    const currentlyOnLast = this.currentSlide === this.imgElements.length - 1;
-
-    this.currentSlide = currentlyOnLast ? 0 : this.currentSlide + 1;
-    this.renderCurrentSlide();
-  }
-
-  prevSlide() {
+  prevSlide(event) {
     const currentlyOnFirst = this.currentSlide === 0;
     const lastSlide = this.imgElements.length - 1;
 
     this.currentSlide = currentlyOnFirst ? lastSlide : this.currentSlide - 1;
-    this.renderCurrentSlide();
+    this.renderCurrentSlide(event);
+  }
+
+  nextSlide(event) {
+    const currentlyOnLast = this.currentSlide === this.imgElements.length - 1;
+
+    this.currentSlide = currentlyOnLast ? 0 : this.currentSlide + 1;
+    this.renderCurrentSlide(event);
   }
 
   generateImgElements() {
@@ -41,6 +46,12 @@ class Carousel {
       imgElement.className = "slide";
       return imgElement;
     });
+  }
+
+  restartSlideInterval() {
+    clearInterval(this.intervalId);
+    this.intervalId = setInterval(() => this.nextSlide(), this.delay);
+    console.log(this.intervalId);
   }
 
   render() {
@@ -55,11 +66,11 @@ class Carousel {
 
     prevSlideBtn.textContent = "<";
     prevSlideBtn.className = "prev-slide";
-    prevSlideBtn.addEventListener("click", () => this.prevSlide());
+    prevSlideBtn.addEventListener("click", (e) => this.prevSlide(e));
 
     nextSlideBtn.textContent = ">";
     nextSlideBtn.className = "next-slide";
-    nextSlideBtn.addEventListener("click", () => this.nextSlide());
+    nextSlideBtn.addEventListener("click", (e) => this.nextSlide(e));
 
     for (let i = 0; i < this.imgElements.length; i++) {
       const btn = document.createElement("button");
@@ -67,13 +78,15 @@ class Carousel {
       btn.className = "jump-to-slide";
       btn.textContent = i + 1;
 
-      btn.addEventListener("click", () => {
+      btn.addEventListener("click", (e) => {
         this.currentSlide = i;
-        this.renderCurrentSlide();
+        this.renderCurrentSlide(e);
       });
 
       jumpToBtnsContainer.appendChild(btn);
     }
+
+    this.intervalId = setInterval(() => this.nextSlide(), this.delay);
 
     this.renderCurrentSlide();
 
@@ -92,8 +105,8 @@ const images = [
   "https://images.unsplash.com/photo-1710831784683-73228be0a085?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   "https://images.unsplash.com/photo-1557227065-79fb4eb2571c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 ];
-const carousel = new Carousel(document.querySelector("#root"), images);
-const carousel2 = new Carousel(document.querySelector("#root"), images);
+const carousel = new Carousel(document.querySelector("#root"), images, 2000);
+const carousel2 = new Carousel(document.querySelector("#root"), images, 500);
 
 carousel.render();
 //carousel2.render();
