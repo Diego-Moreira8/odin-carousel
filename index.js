d071616d329a17371e39dff44eb32ce7cd4b07db
Diego Moreira de Oliveira <diego.moreira529@gmail.com>
2024-03-24 19:54:09 -0300

    feat: display page

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
 import Carousel from "./Carousel.js";
-
-const headerData = [
-  {
-    imgSrc: "./images/dogs-1",
-    imgAlt: "A dog",
-    slideTitle: "A Dog",
-    url: "#",
-  },
-];
-const asideData = [];
-const mainData = [];
-const footerData = [];
+import { headerData, asideData, mainData, footerData } from "./slidesData.js";
 
 const header = document.querySelector("header");
 const aside = document.querySelector("aside");
-const main = document.querySelector(".main-carousel");
+const main = document.querySelector(".carousel-main");
 const footer = document.querySelector("footer");
 
 const headerCarousel = new Carousel(header, headerData, 5000);
-const asideCarousel = new Carousel(aside);
-const mainCarousel = new Carousel(main);
-const footerCarousel = new Carousel(footer);
+const asideCarousel = new Carousel(aside, asideData, 3000);
+const mainCarousel = new Carousel(main, mainData, 4000);
+const footerCarousel = new Carousel(footer, footerData, 6000);
 
 headerCarousel.render();
+asideCarousel.render();
+mainCarousel.render();
+footerCarousel.render();
---
+++ slidesData.js
@@ -0,0 +1,84 @@
+const SLIDE_EXAMPLE = "Slide example";
+
+const headerData = [
+  {
+    imgSrc: "./images/dogs-1.jpg",
+    imgAlt: "Um cachorro lambendo seu focinho",
+    slideTitle: SLIDE_EXAMPLE,
+    url: "#",
+  },
+  {
+    imgSrc: "./images/dogs-2.jpg",
+    imgAlt: "Dois cães correndo numa estrada de terra",
+    slideTitle: SLIDE_EXAMPLE,
+    url: "#",
+  },
+  {
+    imgSrc: "./images/dogs-3.jpg",
+    imgAlt: "Um cachorro dormindo numa cama",
+    slideTitle: SLIDE_EXAMPLE,
+    url: "#",
+  },
+  {
+    imgSrc: "./images/dogs-4.jpg",
+    imgAlt: "Um cachorro de perfil olhando para algo",
+    slideTitle: SLIDE_EXAMPLE,
+    url: "#",
+  },
+  {
+    imgSrc: "./images/dogs-5.jpg",
+    imgAlt: "Dois cães sentados numa rede",
+    slideTitle: SLIDE_EXAMPLE,
+    url: "#",
+  },
+];
+const asideData = [
+  {
+    imgSrc: "./images/cats-1.jpg",
+    imgAlt: "Um gato sorrindo",
+    slideTitle: SLIDE_EXAMPLE,
+    url: "#",
+  },
+  {
+    imgSrc: "./images/cats-2.jpg",
+    imgAlt: "Um gato sentado olhando para o lado",
+    slideTitle: SLIDE_EXAMPLE,
+    url: "#",
+  },
+  {
+    imgSrc: "./images/cats-3.jpg",
+    imgAlt: "Um gato bocejando",
+    slideTitle: SLIDE_EXAMPLE,
+    url: "#",
+  },
+];
+const mainData = [
+  {
+    imgSrc: "./images/capybaras-1.jpg",
+    imgAlt: "Duas capivaras deitadas na grama",
+    slideTitle: SLIDE_EXAMPLE,
+    url: "#",
+  },
+  {
+    imgSrc: "./images/capybaras-2.jpg",
+    imgAlt: "Uma capivara tomando banho num lago",
+    slideTitle: SLIDE_EXAMPLE,
+    url: "#",
+  },
+];
+const footerData = [
+  {
+    imgSrc: "./images/birds-1.jpg",
+    imgAlt: "Três araras azuis num galho",
+    slideTitle: SLIDE_EXAMPLE,
+    url: "#",
+  },
+  {
+    imgSrc: "./images/birds-2.jpg",
+    imgAlt: "Uma ave vermelha num galho com um nevoeiro no fundo",
+    slideTitle: SLIDE_EXAMPLE,
+    url: "#",
+  },
+];
+
+export { headerData, asideData, mainData, footerData };
